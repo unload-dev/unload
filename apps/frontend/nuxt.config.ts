fa93071068e52279b526ac55b3a0e38956a68522
Lukas Drews <info@bytesandbits.net>
2023-04-27 11:18:03 +0200

    Add trpc

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  build: {
+    transpile: ["trpc-nuxt"],
+  },
   app: {
     pageTransition: {
       name: "fade",
