6043ae5b4454976f3515f9163bd9618b46fc3af6
Lukas Drews <info@bytesandbits.net>
2023-05-20 13:38:49 +0200

    Move registry client to repositories

--- apps/frontend/server/trpc/routers/registry.ts
+++ apps/frontend/server/trpc/routers/registry.ts
@@ -1,17 +1,5 @@
 import { z } from "zod";
 import { protectedProcedure, router } from "../trpc";
-import RegistryClient from "@unload/registry-client";
-import { Prisma } from "@prisma/client";
-
-const options = {
-  repository: "localhost:5001",
-  https: true,
-  insecure: true,
-  auth: {
-    username: "testuser",
-    password: "testpassword",
-  },
-};
 
 export const registryRouter = router({
   getAll: protectedProcedure.query(async ({ ctx }) => {
