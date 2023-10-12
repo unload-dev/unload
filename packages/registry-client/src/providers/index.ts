8020e18dd7bbe0dc054ccf8600cdd003ca4ed83f
Lukas Drews <info@bytesandbits.net>
2023-10-12 19:53:42 +0200

    Update the registryProvider interface

--- packages/registry-client/src/providers/index.ts
+++ packages/registry-client/src/providers/index.ts
@@ -1,1 +1,8 @@
-export { DigitalOcean } from "./digitalOcean";
+export { DigitalOceanProvider } from "./digitalOcean";
+export { RegistryProvider, Repository } from "./registryProvider";
+
+export const Provider = {
+  digitalOcean: "DigitalOcean",
+  dockerRegistryV2: "Docker Registry V2",
+} as const;
+export type Provider = (typeof Provider)[keyof typeof Provider];
--- packages/registry-client/src/providers/registryProvider.ts
+++ packages/registry-client/src/providers/registryProvider.ts
@@ -1,8 +1,33 @@
+export interface TokenAuthentication {
+  token: string;
+}
+
+export interface BasicAuthentication {
+  username: string;
+  password: string;
+}
+
+export interface Repository {
+  name: string;
+  manifest_count: number;
+  tag_count: number;
+  last_tag: string;
+  updated_at: Date;
+}
+
 export interface RegistryProvider {
   name: string;
 
+  /**
+   * Supported by:
+   * - Docker Registry V2
+   * - Digital Ocean Registry
+   * - Docker Hub
+   * - GitHub Container Registry
+   *
+   */
   ping(): Promise<boolean>;
-  getRepositories(): Promise<string[]>;
+  getRepositories(): Promise<Repository[]>;
   getTags(repository: string): Promise<string[]>;
   getTag(repository: string, tag: string): Promise<string>;
   deleteTag(repository: string, tag: string): Promise<void>;
