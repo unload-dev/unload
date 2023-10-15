205ee396fb477ab3c50aa06f726271a9a6fa52d8
Lukas Drews <info@bytesandbits.net>
2023-10-15 22:41:40 +0200

    Fix menu border color

--- apps/frontend/components/AppSidebar.vue
+++ apps/frontend/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sticky top-0 hidden h-screen w-48 flex-none flex-col border-r border-gray-100 dark:border-gray-900 px-3 pt-5 pb-4 md:flex lg:w-60"
+    class="sticky top-0 hidden h-screen w-48 flex-none flex-col border-r border-gray-200 dark:border-gray-800 px-3 pt-5 pb-4 md:flex lg:w-60"
   >
     <AppSidebarHeader />
 
